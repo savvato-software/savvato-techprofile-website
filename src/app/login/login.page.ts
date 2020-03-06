eafdc5df4e84764f4b0f4214bebf44ea6ca3d086
Johnathan James <haxwell@gmail.com>
2020-03-06 12:27:31 -0700

    Getting the lost password functionality, such as it is, working.

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -119,7 +119,13 @@ export class LoginPage {
             buttons: [{
               text: "Cancel",
               role: 'cancel'
-            }, {
+            }, { 
+              text: "I Lost It.",
+              handler: () => {
+                self.onLostPasswordClick();
+              }
+            },
+            {
               text: 'GO!',
               handler: (data) => {
                   self._userService.verifyAndLoginUser(u['name'], data.password).then(() => {
@@ -166,205 +172,17 @@ onCancelBtnClicked() {
   this._location.back();
 }
 
-////////
-
-  onLoginBtnTap(event) {
-    if (this.user.name.length > 0 && this.user.password.length > 0) {
-      let self = this;
-
-      this._loadingService.show({
-        message: 'Please wait...'
-      }).then(() => {
-        this._userService.verifyAndLoginUser(this.user.name, this.user.password).then((userObj) => {
-          let pw = self.user.password;
-          let un = self.user.name;
-
-          self.user = userObj;
-          
-          self.user["password"] = pw;
-          self.user["name"] = un;
-
-          self._loadingService.dismiss().then(() => {
-				    this._router.navigate(['/home']);
-          })
-        })
-        .catch((err) => {
-            self._loadingService.dismiss().then(() => {
-              self._alertService.show({header: 'Sad face..',
-                   message: "Bad username/password!",
-                   buttons: [{
-                      text: 'OK',
-                      handler: () => { }
-                    }]
-              });
-            });              
-        });
-      });      
-    }
-  }
-
-  onLostPasswordClick(event) {
+  onLostPasswordClick() {
     let self = this;
     this._alertService.show({
-            header: '',
-            message: "Would you like to reset your password?",
+            header: 'Dangit!',
+            message: "Y'know, ironically, we are really low tech at the moment.<br/><br/>Send an email to info@savvato.com and <br/>we will get you hooked up.",
             buttons: [{
-              text: "Oops, no..",
+              text: "OK",
               role: 'cancel'
-            }, {
-              text: 'Yes!',
-              handler: () => {
-                this.onResetPasswordClick();
-              }
             }]
           })
   }
 
-  onResetPasswordClick() {
-    let self = this;
-      this._alertService.show({
-        header: "Enter your phone number or email address. We'll send you a code.",
-        inputs: [{
-          name: 'phoneNumber',
-          placeholder: '..10 digit phone number..',
-          type: 'number'
-        }],
-        buttons: [{
-          text: 'Cancel',
-          role: 'cancel'
-        }, {
-          text: 'Send Code',
-          handler: (data) => {
-
-            if (data.phoneNumber.length != 10)
-              return false;
-
-            // TODO check for email address too
-            self._userService.isPhoneNumberAvailable(data.phoneNumber).then((isAvailable) => {
-              if (isAvailable) {
-
-                self._alertService.show({
-                        header: 'Uh oh!',
-                        message: "We don't have an account with that phone number. :(",
-                        buttons: [{
-                          text: "OK",
-                          role: 'cancel'
-                        }]
-                      })
-              } else {
-                self._userService.sendCodeToPhoneNumber(data.phoneNumber);
-                self.codeAlreadySent = true;
-
-                this.onCodeSentToPhoneNumber(data.phoneNumber);
-              }
-            });
-          }
-        }]
-    })
-  }
-
-  onCodeSentToPhoneNumber(phoneNumber) {
-    let self = this;
-      this._alertService.show({
-        header: "What's in the text?",
-        inputs: [{
-          name: 'code',
-          placeholder: '..code from text msg..',
-          type: 'number'
-        }],
-        buttons: [{
-          text: 'Cancel',
-          role: 'cancel'
-        }, {
-          text: 'Got it!',
-          handler: (data) => {
-              if (data.code !== undefined && data.code.length > 0) {
-
-                self._userService.isAValidSMSChallengeCode(phoneNumber, data.code).then((b) => {
-                  if (b) {
-
-                      // present dialog allowing user to enter new password
-
-                      self._alertService.show({
-                        header: "Enter Your New Password",
-                        inputs: [{
-                          name: 'pw1',
-                          placeholder: '..new password..'
-                        }, {
-                          name: 'pw2',
-                          placeholder: '..verify password..'
-                        }],
-                        buttons: [{
-                          text: 'Cancel',
-                          role: 'cancel'
-                        }, {
-                          text: 'OK',
-                          handler: (data2) => {
-                            if (data2.pw1 && data2.pw1.length > 5 && data2.pw1 == data2.pw2) {
-                              self._userService.changeLostPassword(data.code, phoneNumber, data2.pw2).then((response) => {
-
-                                  if (response["id"]) {
-                                    self._alertService.show({
-                                      header: 'Yay!',
-                                      message: "Your password has been changed.<br/><br/>Username: " + response["name"],
-                                      buttons: [{
-                                        cssClass: 'finalOkBtn',
-                                        text: 'OK',
-                                        handler: () => {
-
-                                        }
-                                      }]
-                                    })
-                                  } else {
-                                    self._alertService.show({
-                                      header: 'Hmmm...!',
-                                      message: "Could not change your password... Try again.",
-                                      buttons: [{
-                                        text: 'OK',
-                                        handler: () => {
-
-                                        }
-                                      }]
-                                    })
-                                  }
-
-                                }, (err) => {
-                                  
-                                  self._alertService.show({
-                                    header: 'Arggh!',
-                                    message: "Something bad happened on the server. We hate when that happens. Please email us at info@easyah.io and let us know.",
-                                    buttons: [{
-                                      text: 'OK',
-                                      handler: () => {
-                                        
-                                      }
-                                    }]
-                                  })
-                                })
-                            } else {
-                              return false;
-                            }
-                          }
-                        }]
-                    });
-
-                  } else {
-                    self._alertService.show({
-                      header: 'Aargh...',
-                      message: "That wasn't a valid code.......",
-                      buttons: [{
-                        text: 'Grr.',
-                        handler: () => {
-                          
-                        }
-                      }]
-                    })
-                  }
-                })
-              }
-          }
-        }]
-      });
-  }
 
 }

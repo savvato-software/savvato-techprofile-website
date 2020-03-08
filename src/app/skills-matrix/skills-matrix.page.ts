799d61375da32a8deee73e5984f94dcec41fdf0c
Johnathan James <haxwell@gmail.com>
2020-03-08 08:46:05 -0600

    Initial work in getting the historical component to display. ~71% complete.

--- src/app/skills-matrix/skills-matrix.page.ts
+++ src/app/skills-matrix/skills-matrix.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+import { FunctionPromiseService } from 'savvato-javascript-services'
+
+import { environment } from '../../_environments/environment'
 
 @Component({
   selector: 'app-skills-matrix',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsMatrixPage implements OnInit {
 
-  constructor() { }
+	funcKey = "skillsMatrixPageController"
 
-  ngOnInit() {
-  }
+	constructor(private _functionPromiseService: FunctionPromiseService,
+			    private _router: Router,
+			    private _route: ActivatedRoute
+				) { 
+
+	}
+
+	ngOnInit() {
+		let self = this;
+		self._route.params.subscribe((params) => {
+			self._functionPromiseService.initFunc(self.funcKey, () => {
+				return new Promise((resolve, reject) => {
+						resolve({
+							getEnv: () => {
+								return environment;
+							}
+					})
+				})
+			})
+		})
+	}
+
+	getTechProfileUserHistoricalViewController() {
+		return this._functionPromiseService.waitAndGet(this.funcKey, this.funcKey, { });		
+	}
 
 }
